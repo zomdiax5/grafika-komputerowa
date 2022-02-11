2237f5ea09a87a909a1ae360eb5ebd53133a5ca7
zomdiax5 <zomdiax5games@gmail.com>
2022-02-11 15:19:22 +0100

    basic stuff

---
+++ EpicScrollThing.js
@@ -0,0 +1,42 @@
+let oldValue = 0;
+let scroll_direction = "";
+let is_scrolling = false;
+let scrolled = true;
+let frames_scrolled = 0
+let panel = 0
+
+window.scroll(0,0);
+
+window.addEventListener('scroll', (e) => {
+    if (is_scrolling) {
+        return;
+    }
+    if (oldValue < window.scrollY) {
+        is_scrolling = true;
+        panel+=1;
+        frames_scrolled=0
+    } else if (oldValue > window.scrollY) {
+        is_scrolling = true;
+        panel-=1;
+        frames_scrolled=0;
+    };
+});
+setInterval(function () {
+    if(panel < 0){
+        panel=0;
+        is_scrolling=false;
+    }
+    console.log(panel)
+    oldValue = window.scrollY
+    let target_position=window.innerHeight*panel
+    if(!is_scrolling){
+        return;
+    }
+    if(frames_scrolled < 60){
+        frames_scrolled+=1;
+        window.scroll(0, window.scrollY + (target_position-window.scrollY) * 0.080)
+    }
+    else{
+        is_scrolling=false;
+    }
+}, 16.6)
